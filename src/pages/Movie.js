824effbe913e396b21ddf68929e040afe5d73102
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-02 16:57:01 +0200

    affiche la liste de films

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,15 +1,17 @@
 import { useParams } from 'react-router-dom'
-// import filmList from '../filmList.json'
+import filmList from '../filmList.json'
 
 const Movie = () => {
 
   const params = useParams()
+  console.log(params)
+
 
 
   return(
     <>
-      <h1>Title: {params.title}</h1>
-      <h3>Director: {params.director}</h3>
+      <h1>Title: {params.name}</h1>
+      <h3>Director: {}</h3>
       <h3>Stars</h3>
       <ul>
 
