8768f49de9919d97cd0f0feb4d1bb6ae040ecc9b
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-03 10:39:14 +0200

    added style a bit

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,18 +5,27 @@ const HomePage = () => {
 
   return(
     <>
-      <h1>Movie List</h1>
-      <ul>
+      <h1 className='text-center movies-title'>Movies</h1>
+      <section className='d-flex flex-row flex-wrap justify-content-center'>
+
         {filmList.map((film) => {
           return(
-            <li key={film.title}>
-              <Link to={`/movie/${film.title}`}>
-                {film.title}
-              </Link>
-            </li>
+            <>
+              <div className="card col-6 col-sm-4 col-md-3 col-lg-2 m-2 pt-2">
+                <img className="card-img-top" src={film.image} alt="Card image cap"/>
+                <div className="card-body">
+                  <h5 className="w-100 p-0 m-0 text-center">
+                    <Link to={`/movie/${film.id}`}>
+                    {film.title}
+                    </Link>
+                  </h5>
+                </div>
+              </div>
+
+            </>
           )
         })}
-      </ul>
+      </section>
     </>
   )
 }
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,21 +1,31 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import filmList from '../filmList.json'
 
 const Movie = () => {
 
   const params = useParams()
-  console.log(params)
-
-
+  const [film] = useState(filmList[params.name-1])
 
   return(
     <>
-      <h1>Title: {params.name}</h1>
-      <h3>Director: {}</h3>
-      <h3>Stars</h3>
-      <ul>
-
-      </ul>
+      <h1 className='text-center film-title pt-3'>{film.title}</h1>
+      <section className='d-flex flex-column align-items-center justify-content-center'>
+        <img className="col-8 col-md-4 col-lg-3 film-image my-3" src={film.image} alt="Card image cap"/>
+        <div className='col-8 text-white'>
+          <h3>Directed by: {film.director}</h3>
+          <h3>Stars</h3>
+          <ul>
+            {film.stars.map((star) => {
+              return(
+                <li key={star}>{star}</li>
+                )
+              })}
+          </ul>
+          <h3>Description</h3>
+          <p>{film.description}</p>
+        </div>
+      </section>
     </>
   )
 }
